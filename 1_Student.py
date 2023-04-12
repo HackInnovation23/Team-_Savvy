2e29e6c6fbabc169b93cf7f6ecf0ce66b2cc67aa
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-04-12 13:22:19 +0530

    Update 1_Student.py

--- 1_Student.py
+++ 1_Student.py
@@ -181,7 +181,7 @@ pipe = pkl.load(data)
 
 
 
-option = ['Semester Result Precdition','Placement Prediction']
+option = ['Semester Result Prediction','Placement Prediction']
 
 
 with st.sidebar:
@@ -192,7 +192,7 @@ with st.sidebar:
 
 
 if selected == option[0]:
-    st.title("Semester Result Precdition:")
+    st.title("Semester Result Prediction:")
     c,c0 = st.columns([2,2])
 
     c1,c2,c3 = st.columns([3,1,3])
